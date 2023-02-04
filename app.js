a8f64209e1cabeacf1d76d2c6c9dea83213ce8d8
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-02-04 12:42:55 +0100

    Create a module function that creates the game grid and assign classes and ids to its elements

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('working');
+const gameDisplay = (() => {
+    const gameBoard = ['', '', '', '', '', '', '', '', ''];
+    const gameGrid = (() => {
+        const grid = document.createElement('div');
+        grid.classList.add('main-grid');
+        for (let i = 0; i < 9; i += 1) {
+            const gridElements = document.createElement('div');
+            gridElements.classList.add('inner-grid');
+            gridElements.id = `gridElement-${i}`;
+            grid.appendChild(gridElements);
+        } return grid;
+    })(); const gridContainer = document.getElementById('grid-container'); gridContainer.appendChild(gameGrid);
+    return gameBoard;
+})();
+console.log(gameDisplay);
+const playerFactory = (player1, player2) => {
+    const sayHello = () => console.log('hello');
+    return { player1, player2, sayHello };
+};
+
+const humanPlayer = playerFactory('1', '2');
+humanPlayer.sayHello();
