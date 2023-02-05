fb8d75db41231763132928f5b43a06f551cb0df5
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-02-05 12:59:13 +0100

    Create a function factory that takes user choise into consideration and pushes it into the gameboard Array

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const gameDisplay = (() => {
-    const gameBoard = ['', '', '', '', '', '', '', '', ''];
-    const gameGrid = (() => {
-        const grid = document.createElement('div');
-        grid.classList.add('main-grid');
-        grid.id = 'main-grid';
+const gameBoard = (() => {
+    const gameBoard3x3grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    return gameBoard3x3grid;
+})();
+
+const gameGrid = (() => {
+    const grid = (() => {
+        const mainGrid = document.createElement('div');
+        mainGrid.classList.add('main-grid');
+        mainGrid.id = 'main-grid';
         for (let index = 0; index < 9; index += 1) {
             const gridElements = document.createElement('div');
             gridElements.classList.add('inner-grid');
             gridElements.id = `gridElement-${index}`;
-            grid.appendChild(gridElements);
-        } return grid;
-    })(); const gridContainer = document.getElementById('grid-container'); gridContainer.appendChild(gameGrid);
-    return gameBoard;
+            mainGrid.appendChild(gridElements);
+        }
+        return mainGrid;
+    })();
+    const gridContainer = document.getElementById('grid-container'); gridContainer.appendChild(grid);
+    return grid;
 })();
 
+const gameFactory = (choise, position) => {
+    const displayChoise = () => document.getElementById(position);
+    const addChoiseToGameArr = () => gameBoard.push(choise);
+    console.log(position, choise);
+    return { displayChoise, addChoiseToGameArr };
+};
+
 const playerSelection = (() => {
-    const gameGrid = document.getElementById('main-grid');
-    gameGrid.addEventListener('click', (event) => {
+    const choiseOne = 'x';
+    const choiseTwo = 'o';
+    const gridEvent = document.getElementById('main-grid');
+    gridEvent.addEventListener('click', (event) => {
         event.target.classList.add('user-selection');
+        const playerMove = gameFactory(choiseOne, event.target);
+        playerMove.displayChoise();
+        playerMove.addChoiseToGameArr();
+        console.log(gameBoard);
     });
 })();
 
@@ -31,3 +54,5 @@ const player1 = playerFactory('john');
 const player2 = playerFactory('Adams');
 player1.player1Displayed();
 player2.player2Displayed();
+
+// next to figure out is how to add the choise to the specific index of an array
