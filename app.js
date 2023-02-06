29cf12b38ffe08162562045d491493ea209fe0a4
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-02-06 14:14:49 +0100

    Assign the value of the players selection to corresponding index of the gameboard Array

--- app.js
+++ app.js
@@ -11,17 +11,16 @@ const playerSelection = (() => {
     const gridEvent = document.getElementById('gameboard');
     let playerMove = 0;
     gridEvent.addEventListener('click', (event) => {
-        console.log(playerMove);
+        const boardElementId = event.target.id;
+        console.log(gameBoard);
         if (playerMove === 0 && !event.target.classList.contains('user-o')) {
+            gameBoard[boardElementId] = 'x'; // Board element id corresponds to the location of the gameBoard index
             event.target.classList.add('user-x');
             playerMove += 1;
         } else if (playerMove === 1 && !event.target.classList.contains('user-x')) {
+            gameBoard[boardElementId] = 'o'; // Board element id corresponds to the location of the gameBoard index
             event.target.classList.add('user-o');
             playerMove -= 1;
         }
     });
 })();
-
-const ticTacToeGame = (player1, player2) => {
-
-};
